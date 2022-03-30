702613e15bf04b93a465c3135d280be24a9692f9
AliAbuRub <90558669+AliAbuRub@users.noreply.github.com>
2022-03-30 03:52:46 +0300

    Tuesday Work

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'gray': '#878b98',
+        'light-gray': '#e8e8e8',
+      },
+    },
+  },
+  plugins: [],
+}
